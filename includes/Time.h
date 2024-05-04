943a38b4f2452be5fbb043f40f130c7d100c05b4
realhungryhuman <61504185+realhungryhuman@users.noreply.github.com>
2024-05-04 16:15:18 +0800

    Revert "Implement Comparision Operator Overloades"
    This reverts commit 9066713f4bbb36e054cbc631bf7b161623fcbff3.

--- includes/Time.h
+++ includes/Time.h
@@ -17,10 +17,6 @@
 	 * @author 34085068
 	 * @version 01
 	 * @date 08/04/2024 34085068, Started
-	 *
-	 * @author 34085068
-	 * @version 02
-	 * @date 04/05/2024 34085068, Add comparison operator overloads as Times have a natural order.
 	 */
 class Time
 {
@@ -95,63 +91,9 @@ class Time
              * This ensures Seconds is always valid.
              *
              * @param secs - Value of Seconds
-             * @return voidTime
+             * @return void
              */
         void SetSeconds(int secs);
-
-            /**
-             * @brief  Compares dates for equality.
-             *
-             *
-             * @param right - Time object to be compared against
-             * @return bool - Returns true if Dates are Equal. Otherwise returns false.
-             */
-        bool operator==(Time &right) const;
-
-            /**
-             * @brief  Checks if Time is less than another Time
-             *
-             *
-             * @param right - Time object to be compared against
-             * @return bool - Returns true if Time is less than another Time. Otherwise returns false.
-             */
-        bool operator<(Time &right) const;
-
-            /**
-             * @brief  Checks if Time is less than or equal to another Time.
-             *
-             *
-             * @param right - Time object to be compared against
-             * @return bool - Returns true if Time is less than or equal too another Time. Otherwise returns false.
-             */
-        bool operator<=(Time &right) const;
-
-            /**
-             * @brief  Checks if Time is greater than another Time.
-             *
-             *
-             * @param right - Time object to be compared against
-             * @return bool - Returns true if Time is greater than another Time. Otherwise returns false.
-             */
-        bool operator>(Time &right) const;
-
-            /**
-             * @brief  Checks if Time is greater than or equal to another Time.
-             *
-             *
-             * @param right - Time object to be compared against
-             * @return bool - Returns true if Time is less than or equal too another Time. Otherwise returns false.
-             */
-        bool operator>=(Time &right) const;
-
-            /**
-             * @brief  Compares dates for inequality.
-             *
-             *
-             * @param right - Time object to be compared against
-             * @return bool - Returns true if Dates are not Equal. Otherwise returns false.
-             */
-        bool operator!=(Time &right) const;
 
     private:
             /// Hours: Values 0-23
