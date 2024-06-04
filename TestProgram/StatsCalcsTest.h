4beaedcf5c450d0ec235fcecf345f85e395f2c7e
realhungryhuman <61504185+realhungryhuman@users.noreply.github.com>
2024-06-04 18:06:44 +0800

    Delete TestProgram directory

--- TestProgram/StatsCalcsTest.h
+++
@@ -1,8 +0,0 @@
-#ifndef STANDARDDEVTEST_H_INCLUDED
-#define STANDARDDEVTEST_H_INCLUDED
-
-int TestCalcSumOfVectorf();
-int TestCalcMeanOfVectorf();
-int TestCalcStdDevOfVectorf();
-
-#endif // STANDARDDEVTEST_H_INCLUDED
